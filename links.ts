a42b4b6e6e04498a3fe4fe6bbd5d872afcdc69a0
Jesse Conner <jconner90@gmail.com>
2021-06-05 20:17:50 -0400

    Lint things

--- links.ts
+++ links.ts
@@ -1,44 +1,43 @@
-import URLTools from './urls';
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+import URLTools from './urls'
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
 
-const allowedFileList = [`php`, `asp`, `htm`, `html`, `aspx`, ``];
-const allowedProtocolList = [`http`, `https`, ``];
+const allowedFileList = ['php', 'asp', 'htm', 'html', 'aspx', '']
+const allowedProtocolList = ['http', 'https', '']
 
-function find(body: string, domain: string, current?: string): Array<string> {
-	
-	const dom = new JSDOM(body);
-	const doc = dom.window.document;
-	const anchors = doc.querySelectorAll(`a`);
+function find (body: string, domain: string, current?: string): Array<string> {
+  const dom = new JSDOM(body)
+  const doc = dom.window.document
+  const anchors = doc.querySelectorAll('a')
 
-	// const re = /href=[\"\']?([^\"\'\ \>]+)[\"\']?/g;
-	const results: Array<string> = [];
-	// let match:RegExpExecArray;
-	//while ((match = re.exec(body)) !== null) {
-	for (const result of anchors) {
-		const match = result.href;
-		const link = URLTools.cleanLink(match, domain, current ?? ``);
-		if (!link) continue;
-		if (!allowedFileList.includes(URLTools.findFileType(link))) continue;
-		if (!allowedProtocolList.includes(URLTools.findProtocol(link))) continue;
-		if (URLTools.findDomain(link).includes(`:`) || URLTools.findPath(link).includes(`:`) ) continue;
-		results.push(link);
-	}
-	return results;
+  // const re = /href=[\"\']?([^\"\'\ \>]+)[\"\']?/g;
+  const results: Array<string> = []
+  // let match:RegExpExecArray;
+  // while ((match = re.exec(body)) !== null) {
+  for (const result of anchors) {
+    const match = result.href
+    const link = URLTools.cleanLink(match, domain, current ?? '')
+    if (!link) continue
+    if (!allowedFileList.includes(URLTools.findFileType(link))) continue
+    if (!allowedProtocolList.includes(URLTools.findProtocol(link))) continue
+    if (URLTools.findDomain(link).includes(':') || URLTools.findPath(link).includes(':')) continue
+    results.push(link)
+  }
+  return results
 }
 
-function findInternal(body: string, domain: string, current: string): Array<string> {
-	const allLinks = find(body, domain, current);
-	return allLinks.filter(link => { return URLTools.isInternal(link, domain) });
+function findInternal (body: string, domain: string, current: string): Array<string> {
+  const allLinks = find(body, domain, current)
+  return allLinks.filter(link => { return URLTools.isInternal(link, domain) })
 }
 
-function findExternal(body: string, domain: string): Array<string> {
-	const allLinks = find(body, domain);
-	return allLinks.filter(link => {return !URLTools.isInternal(link, domain)});
+function findExternal (body: string, domain: string): Array<string> {
+  const allLinks = find(body, domain)
+  return allLinks.filter(link => { return !URLTools.isInternal(link, domain) })
 }
 
 export default {
-	find,
-	findInternal,
-	findExternal,
+  find,
+  findInternal,
+  findExternal
 }
--- pages.ts
+++ pages.ts
@@ -1,46 +1,41 @@
-import LinkFinder from './links';
+import LinkFinder from './links'
 import Request from './requests'
 
 export default class Page {
+    target: string;
+    domain: string;
+    source?: string;
+    responseCode?: number;
+    body?: string;
 
-	target: string;
-	domain: string;
-	source?: string;
-	responseCode?: number;
-	body?: string;
+    constructor (target: string, domain: string, source?: string) {
+      this.target = target
+      this.domain = domain
+      this.source = source ?? ''
+    }
 
-	constructor(target: string, domain: string, source?: string) {
-		this.target = target;
-		this.domain = domain;
-		this.source = source ?? '';
-	}
+    async get (): Promise<void> {
+      try {
+        const response = await Request(this.target)
+        this.body = response.body
+        this.responseCode = response.code
+      } catch (err) {
+        this.responseCode = err.status
+      }
+    }
 
-	async get(): Promise<void> {
+    links (): Array<string> {
+      if (!this.body) return null
+      return LinkFinder.find(this.body, this.domain, this.target)
+    }
 
-		try {
-			const response = await Request(this.target);
-			this.body = response.body;
-			this.responseCode = response.code;
-			
-		} catch (err) {
-			this.responseCode = err.status;
-		}
-	
-	}
+    internalLinks (): Array<string> {
+      if (!this.body) return null
+      return LinkFinder.findInternal(this.body, this.domain, this.target)
+    }
 
-	links(): Array<string> {
-		if (!this.body) return null;
-		return LinkFinder.find(this.body, this.domain, this.target);
-	}
-
-	internalLinks(): Array<string> {
-		if (!this.body) return null;
-		return LinkFinder.findInternal(this.body, this.domain, this.target);
-	}
-
-	externalLinks(): Array<string> {
-		if (!this.body) return null;
-		return LinkFinder.findExternal(this.body, this.domain);
-	}
-
+    externalLinks (): Array<string> {
+      if (!this.body) return null
+      return LinkFinder.findExternal(this.body, this.domain)
+    }
 }
