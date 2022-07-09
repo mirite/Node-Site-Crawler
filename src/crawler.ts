36fad854137ee96e80db200d0115e0d7ef59b2bd
Jesse Conner <jconner90@gmail.com>
2022-07-09 17:35:21 -0400

    Linting

--- src/crawler.ts
+++ src/crawler.ts
@@ -11,101 +11,101 @@ export type Status = {
 }
 
 export class Crawler {
-    events: EventEmitter
+  events: EventEmitter;
 
-    myReqs: Array<Page>
+  myReqs: Array<Page>;
 
-    status: Status
+  status: Status;
 
-    domain: string
+  domain: string;
 
-    constructor(domain: string) {
-      this.events = new EventEmitter();
-      this.domain = domain;
+  constructor(domain: string) {
+    this.events = new EventEmitter();
+    this.domain = domain;
 
-      this.myReqs = [];
+    this.myReqs = [];
 
-      this.status = {
-        pagesCrawled: 0,
-        currentPage: '',
-        isDone: false,
-      };
+    this.status = {
+      pagesCrawled: 0,
+      currentPage: '',
+      isDone: false,
+    };
+  }
+
+  updateStatus(): void {
+    this.events.emit('update', this.status);
+  }
+
+  async crawlSite(): Promise<void> {
+    const homepage = new Page(`https://${this.domain}/`, this.domain);
+    await this.crawlPage(homepage);
+    this.status.isDone = true;
+    this.updateStatus();
+    this.writeResults();
+  }
+
+  getCrawled():Array<string> {
+    const crawled: Array<string> = [];
+    this.myReqs.forEach((request) => {
+      crawled.push(request.target);
+    });
+    return crawled;
+  }
+
+  async crawlPage(current: Page) {
+    this.status.pagesCrawled += 1;
+    this.status.currentPage = current.target;
+    this.updateStatus();
+    await current.get();
+
+    const internalLinks = current.internalLinks();
+    if (!internalLinks) return;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const link of internalLinks) {
+      // eslint-disable-next-line no-await-in-loop
+      await this.processLink(link, current);
     }
+  }
 
-    updateStatus(): void {
-      this.events.emit('update', this.status);
-    }
-
-    async crawlSite(): Promise<void> {
-      const homepage = new Page(`https://${this.domain}/`, this.domain);
-      await this.crawlPage(homepage);
-      this.status.isDone = true;
-      this.updateStatus();
-      this.writeResults();
-    }
-
-    getCrawled():Array<string> {
-      const crawled: Array<string> = [];
-      this.myReqs.forEach((request) => {
-        crawled.push(request.target);
-      });
-      return crawled;
-    }
-
-    async crawlPage(current: Page) {
-      this.status.pagesCrawled += 1;
-      this.status.currentPage = current.target;
-      this.updateStatus();
-      await current.get();
-
-      const internalLinks = current.internalLinks();
-      if (!internalLinks) return;
-      // eslint-disable-next-line no-restricted-syntax
-      for (const link of internalLinks) {
-        // eslint-disable-next-line no-await-in-loop
-        await this.processLink(link, current);
+  async processLink(link: string, current: Page):Promise<void> {
+    let skip = false;
+    this.myReqs.forEach((request) => {
+      if (request.target === link) {
+        request.sources.push(current.target);
+        skip = true;
       }
-    }
+    });
+    if (skip) return;
+    const newReq = new Page(link, current.domain, current.target);
+    this.myReqs.push(newReq);
+    await this.crawlPage(newReq);
+  }
 
-    async processLink(link: string, current: Page):Promise<void> {
-      let skip = false;
-      this.myReqs.forEach((request) => {
-        if (request.target === link) {
-          request.sources.push(current.target);
-          skip = true;
-        }
-      });
-      if (skip) return;
-      const newReq = new Page(link, current.domain, current.target);
-      this.myReqs.push(newReq);
-      await this.crawlPage(newReq);
+  writeResults(): void {
+    const path = this.makePathSafe();
+    if (!fs.existsSync('crawls')) {
+      fs.mkdirSync('crawls');
     }
-
-    writeResults(): void {
-      const path = this.makePathSafe();
-      if (!fs.existsSync('crawls')) {
-        fs.mkdirSync('crawls');
-      }
-      if (!fs.existsSync(`crawls/${path}`)) {
-        fs.mkdirSync(`crawls/${path}`);
-      }
-      fs.writeFileSync(`crawls/${path}.json`, JSON.stringify(this.myReqs));
-      let i = 0;
-      this.myReqs.forEach((req) => {
-        fs.writeFileSync(`crawls/${path}/${i}.json`, JSON.stringify(req));
-        i += 1;
-      });
+    if (!fs.existsSync(`crawls/${path}`)) {
+      fs.mkdirSync(`crawls/${path}`);
     }
+    fs.writeFileSync(`crawls/${path}.json`, JSON.stringify(this.myReqs));
+    let i = 0;
+    this.myReqs.forEach((req) => {
+      fs.writeFileSync(`crawls/${path}/${i}.json`, JSON.stringify(req));
+      i += 1;
+    });
+  }
 
-    makePathSafe():string {
-      return this.domain.replace(/\./g, '_');
-    }
+  makePathSafe():string {
+    return this.domain.replace(/\./g, '_');
+  }
 
-    loadResults(): Array<Page> {
-      const path = this.makePathSafe();
+  loadResults(): Array<Page> {
+    const path = this.makePathSafe();
 
-      const raw = fs.readFileSync(`crawls/${path}.json`);
-      const body = JSON.parse(raw.toString());
-      return body;
-    }
+    const raw = fs.readFileSync(`crawls/${path}.json`);
+    const body = JSON.parse(raw.toString());
+    return body;
+  }
 }
